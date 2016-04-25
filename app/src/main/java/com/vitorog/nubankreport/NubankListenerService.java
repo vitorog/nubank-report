e61b63c8211591e5f5d4693e0a32b4c778db1824
vgomes <vgomes@motorola.com>
2016-04-25 18:45:32 -0300

    --Create receiver class

--- app/src/main/java/com/vitorog/nubankreport/NubankListenerService.java
+++ app/src/main/java/com/vitorog/nubankreport/NubankListenerService.java
@@ -1,12 +1,21 @@
 package com.vitorog.nubankreport;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
 /**
  * Created by vitor.gomes on 25/04/2016.
  */
-public class NubankNotificationListenerService extends NotificationListenerService {
+public class NubankListenerService extends NotificationListenerService {
+
+    private NubankListenerServiceReceiver serviceReceiver;
+
+    @Override
+    public void onCreate() {
+        serviceReceiver = new NubankListenerServiceReceiver();
+    }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
---
+++ app/src/main/java/com/vitorog/nubankreport/NubankListenerServiceReceiver.java
@@ -0,0 +1,15 @@
+package com.vitorog.nubankreport;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by vitor.gomes on 25/04/2016.
+ */
+public class NubankListenerServiceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
